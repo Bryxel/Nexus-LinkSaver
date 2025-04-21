ac6996b058976ac667082d811524fd3c59409253
Bryxel Fuentes <72637926+Bryxel@users.noreply.github.com>
2025-04-21 05:14:09 -0400

    fixed static file config

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 
 
-import environ
+import environ, os
 
 env = environ.Env()
 
@@ -130,8 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
